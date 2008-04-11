e352f033aaeab34cf529fca816c0efde9a1ef518
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 20:52:27 +0000

    javadoc updates

--- src/rutebaga/model/entity/npc/NPCState.java
+++ src/rutebaga/model/entity/npc/NPCState.java
@@ -6,24 +6,83 @@ import rutebaga.model.entity.npc.state.Evade;
 import rutebaga.model.entity.npc.state.Wander;
 
 /**
- * @author nicholasstamas
- *
+ * Encapsulates state-specific behavior of a {@link ConcreteNPCBrain} and
+ * defines state transitions. For example, a default state might be wander, but
+ * hostile action might facilitate a transition to the chase or the attack
+ * state.
+ * 
+ * @author Nicholas Stamas
+ * @see ConcreteNPCBrain
+ * @see NPCEntity
  */
 public abstract class NPCState {
 	static NPCState initialState;
+
 	protected static final NPCState wander = new Wander();
+	
+	// XXX: because it's
+	// the first state,
+	// the initialState will be wander. Probably not too good.
+
 	protected static final NPCState chase = new Chase();
+
 	protected static final NPCState evade = new Evade();
+
 	protected static final NPCState attack = new Attack();
-	
+
 	protected NPCState() {
-		if (initialState == null) initialState = this;
+		if (initialState == null)
+			initialState = this;
 	}
-	
-	public abstract NPCState tick( NPCEntity npc );
-	public abstract NPCState speak( NPCEntity npc );
-	public abstract NPCState barter( NPCEntity npc );
-	public abstract NPCState makeHostile( NPCEntity npc );
-	public abstract NPCState makeFriendly( NPCEntity npc );
-	
+
+	/**
+	 * Returns the state that results from a tick.
+	 * 
+	 * @param npc
+	 *            The {@link NPCEntity} whos state is in question.
+	 * @return The resulting NPCState.
+	 * @see NPCEntity
+	 */
+	public abstract NPCState tick(NPCEntity npc);
+
+	/**
+	 * Returns the state that results from speaking.
+	 * 
+	 * @param npc
+	 *            The {@link NPCEntity} whos state is in question.
+	 * @return The resulting NPCState.
+	 * @see NPCEntity
+	 */
+	public abstract NPCState speak(NPCEntity npc);
+
+	/**
+	 * Returns the state that results from bartering.
+	 * 
+	 * @param npc
+	 *            The {@link NPCEntity} whos state is in question.
+	 * @return The resulting NPCState.
+	 * @see NPCEntity
+	 */
+	public abstract NPCState barter(NPCEntity npc);
+
+	/**
+	 * Returns the state that results from becoming hostile.
+	 * 
+	 * @param npc
+	 *            The {@link NPCEntity} whos state is in question.
+	 * @return The resulting NPCState.
+	 * @see NPCEntity
+	 */
+	public abstract NPCState makeHostile(NPCEntity npc);
+
+	/**
+	 * Returns the state that results from becoming friendly.
+	 * 
+	 * @param npc
+	 *            The {@link NPCEntity} whos state is in question.
+	 * @return The resulting NPCState.
+	 * @see NPCEntity
+	 */
+	public abstract NPCState makeFriendly(NPCEntity npc);
+
 }
