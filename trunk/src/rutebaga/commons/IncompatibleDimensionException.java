6e12e92f4773681c5167ca2302312ae47c9047a8
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 00:04:42 +0000

    javadoc for IncompatibleDimensionsException

--- trunk/src/rutebaga/commons/IncompatibleDimensionException.java
+++ trunk/src/rutebaga/commons/IncompatibleDimensionException.java
@@ -1,5 +1,11 @@
 package rutebaga.commons;
 
+/**
+ * An Exception thrown by {@link Vector#check(Vector)} when a Vector operation cannot
+ * be completed because of incompatible dimensions.
+ * @author Gary
+ *
+ */
 public class IncompatibleDimensionException extends RuntimeException {
 	
 	private static final long serialVersionUID = -2884085458913564910L;
