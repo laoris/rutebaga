9a4e20b8d8e0a251943958c9ee03befaeaf9e07a
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 01:51:42 +0000

    javadocs

--- trunk/src/rutebaga/commons/Bounds.java
+++ trunk/src/rutebaga/commons/Bounds.java
@@ -4,21 +4,33 @@ package rutebaga.commons;
  * Interface for declaring a bounds within space.
  * 
  * @author Gary LosHuertos
- *
+ * 
  */
-public abstract class Bounds
-{
+public abstract class Bounds {
 	public abstract boolean contains(Vector v);
-	public Matrix filter(Matrix m)
-	{
+
+	/**
+	 * Tests if the {@link Vector}s that are the columns in {@link Matrix}
+	 * m are contained by these Bounds and returns another Matrix who's columns
+	 * are only the {@link Vector}s from {@link Matrix} m that are contained in
+	 * these Bounds.
+	 * 
+	 * @param m
+	 *            {@link Matrix} who's columns are {@link Vector}s to be tested
+	 *            for containment in these Bounds.
+	 * @return The {@link Matrix} who's columns are {@link Vector}s that exist
+	 *         within these Bounds.
+	 *        @see Matrix
+	 *        @see Vector
+	 */
+	public Matrix filter(Matrix m) {
 		Vector[] vectors = m.asVectors();
-		
-		for(int idx=0; idx<vectors.length; idx++)
-		{
-			if(!contains(vectors[idx]))
+
+		for (int idx = 0; idx < vectors.length; idx++) {
+			if (!contains(vectors[idx]))
 				vectors[idx] = null;
 		}
-		
+
 		return new Matrix(vectors);
 	}
 }
