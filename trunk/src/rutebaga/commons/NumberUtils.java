632b6a5b75ea8cca1c9c985886b79202c23d4103
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 02:18:38 +0000

    javadocs

--- trunk/src/rutebaga/commons/NumberUtils.java
+++ trunk/src/rutebaga/commons/NumberUtils.java
@@ -1,7 +1,19 @@
 package rutebaga.commons;
 
+/**
+ * Simple numeric methods.
+ * @author Gary
+ *
+ */
 public class NumberUtils
 {
+	/**
+	 * Returns true if the value given falls between the maximum and minimum values provided.
+	 * @param value The number to test.
+	 * @param min The minimum value.
+	 * @param max The maximum value.
+	 * @return The boolean that is true if the value is greater than min and less than max.
+	 */
 	public static boolean between(double value, double min, double max)
 	{
 		return value < max && value >= min;
--- trunk/src/rutebaga/commons/StaticRule.java
+++ trunk/src/rutebaga/commons/StaticRule.java
@@ -1,21 +1,49 @@
 package rutebaga.commons;
 
-public class StaticRule<T> implements Rule<T>
-{
+/**
+ * Used to represent a rule that should always evaluate the same way.
+ * 
+ * @author Gary
+ * 
+ * @param <T>
+ *            The context for this Rule.
+ */
+public class StaticRule<T> implements Rule<T> {
 	private final boolean value;
 
-	public StaticRule(boolean value)
-	{
+	/**
+	 * Constructs a StaticRule that will always evaluate to the provided value.
+	 * 
+	 * @param value
+	 *            will define the evaluation of this StaticRule.
+	 */
+	public StaticRule(boolean value) {
 		this.value = value;
 	}
 
-	public boolean isValue()
-	{
+	/**
+	 * Returns the static value of this StaticRule. The same as calling
+	 * {@link #determine(T)}.
+	 * 
+	 * @return the value of this StaticRule. The same as evaluating
+	 *         {@link #determine(T)}
+	 * @see #determine(T)
+	 */
+	public boolean isValue() {
 		return value;
 	}
 
-	public boolean determine(T context)
-	{
+	/**
+	 * Returns the evaluation of this StaticRule in the provided context. The
+	 * same as calling {@link #isValue()}.
+	 * 
+	 * @param context
+	 *            The context in which this StaticRule will be evaluated.
+	 * @return The evaluation of this StaticRule in the provided context.
+	 * @see rutebaga.commons.Rule#determine(null)
+	 * @see #isValue()
+	 */
+	public boolean determine(T context) {
 		return value;
 	}
 
