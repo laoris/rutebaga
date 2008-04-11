c973ef65511ce2c0bffef0ba5e934890a0076125
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 17:36:25 +0000

    javadocs

--- trunk/src/rutebaga/model/entity/Entity.java
+++ trunk/src/rutebaga/model/entity/Entity.java
@@ -1,5 +1,19 @@
 package rutebaga.model.entity;
 
+/**
+ * Entity stores the state related to an Entity in a physical environment. Entities
+ * are objects defined by certain anthropomorphic attributes, such as containing
+ * stats and an inventory. However, Entities are not necessarily anthropomorphic
+ * themselves.
+ * 
+ * Entity is defined by its {@link EntityType}, to which it forwards most of its
+ * behavior. Entity exists only to retain state. Subclasses of Entity that
+ * require new operations should defer the execution of those operations to
+ * their own subclass of {@link EntityType}.
+ * 
+ * @author Nick
+ * @see EntityType
+ */
 public class Entity {
 
 }
--- trunk/src/rutebaga/model/entity/npc/NPCBrain.java
+++ trunk/src/rutebaga/model/entity/npc/NPCBrain.java
@@ -1,17 +1,54 @@
 package rutebaga.model.entity.npc;
 
 /**
- * Interface for an NPCBrain.
+ * NPCBrain specifies the interface that a {@link ConcreteNPCBrain} must
+ * support, allowing seamless swapping of different ConcreteNPCBrains. The
+ * operations it supports are abstractions of inputs to an NPC brain.
  * 
- * @author nicholasstamas
- *
+ * @author Nicholas Stamas
+ * @see ConcreteNPCBrain
  */
 public interface NPCBrain {
 
-	public void tick( NPCEntity npc );
-	public void speak( NPCEntity npc );
-	public void barter( NPCEntity npc );
-	public void makeHostile( NPCEntity npc );
-	public void makeFriendly( NPCEntity npc );
-	
+	/**
+	 * A method called on by the {@link NPCEntity} owning this NPCBrain to
+	 * decide what to do at each clock tick.
+	 * @see NPCEntity
+	 * @param npc
+	 *            The {@link NPCEntity} controlled by this NPCBrain.
+	 */
+	public void tick(NPCEntity npc);
+
+	/**
+	 * A method called on by the {@link NPCEntity} owning this NPCBrain to
+	 * decide what to do when speaking.
+	 * @param npc
+	 *            The {@link NPCEntity} controlled by this NPCBrain.
+	 */
+	public void speak(NPCEntity npc);
+
+	/**
+	 * A method called on by the {@link NPCEntity} owning this NPCBrain to
+	 * decide what to do when bartering.
+	 * @param npc
+	 *            The {@link NPCEntity} controlled by this NPCBrain.
+	 */
+	public void barter(NPCEntity npc);
+
+	/**
+	 * A method called on by the {@link NPCEntity} owning this NPCBrain to
+	 * decide what to do when made hostile.
+	 * @param npc
+	 *            The {@link NPCEntity} controlled by this NPCBrain.
+	 */
+	public void makeHostile(NPCEntity npc);
+
+	/**
+	 * A method called on by the {@link NPCEntity} owning this NPCBrain to
+	 * decide what to do when made friendly.
+	 * @param npc
+	 *            The {@link NPCEntity} controlled by this NPCBrain.
+	 */
+	public void makeFriendly(NPCEntity npc);
+
 }
