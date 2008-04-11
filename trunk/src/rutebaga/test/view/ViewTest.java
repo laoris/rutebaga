eb888a4ca4fc6ccfcff89ef24ac9c97bbc2e0b32
rcobb85 <rcobb85@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 00:54:42 +0000

    Added a View Test package.

---
+++ trunk/src/rutebaga/test/view/ViewTest.java
@@ -0,0 +1,26 @@
+package rutebaga.test.view;
+
+import java.awt.Color;
+import java.awt.Font;
+
+import rutebaga.view.FPSTextComponent;
+import rutebaga.view.rwt.TextLabelComponent;
+import rutebaga.view.rwt.View;
+
+public class ViewTest {
+
+	public static void main(String args[]) {
+		View view = new View( 800, 600, true);
+		
+		
+		TextLabelComponent text = new FPSTextComponent();
+		text.setLocation(100,100);
+		text.setFontColor( Color.BLUE );
+		text.setFont(new Font("Times New Roman", Font.PLAIN, 20));
+		
+		view.addViewComponent(text);
+		
+		while(true)
+			view.renderFrame();
+	}
+}
