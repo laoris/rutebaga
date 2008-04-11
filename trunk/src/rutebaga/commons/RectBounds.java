f32469f04d1d76c93329a4458b98f92511800223
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 01:25:28 +0000

    Javadocs for rectbounds

--- trunk/src/rutebaga/commons/RectBounds.java
+++ trunk/src/rutebaga/commons/RectBounds.java
@@ -1,23 +1,39 @@
 package rutebaga.commons;
 
-public class RectBounds extends Bounds
-{
+public class RectBounds extends Bounds {
 	private Vector size;
-	private Vector lower;
 
-	public RectBounds(Vector lower, Vector size)
-	{
+	private Vector lower;// lower left corner
+
+	/**
+	 * Constructs a RectBounds based on the specified lower-left corner and
+	 * size.
+	 * 
+	 * @param lower
+	 *            {@link Vector} representing the location of the lower-left
+	 *            corner of this RectBounds.
+	 * @param size
+	 *            {@link Vector} representing the size of this RectBounds. This
+	 *            {@link Vector} should point to the upper-right corner of the
+	 *            RectBounds.
+	 *            @see Vector
+	 */
+	public RectBounds(Vector lower, Vector size) {
 		this.size = size;
 		this.lower = lower;
 	}
 
-	public boolean contains(Vector v)
-	{
+	/**
+	 * Returns true if this RectBounds contains the specified {@link Vector}.
+	 * @param v {@link Vector} to be checked.
+	 * @return A boolean representing {@link Vector} v's containment in this RectBounds.
+	 * @see Vector
+	 */
+	public boolean contains(Vector v) {
 		boolean contains = true;
 		// each element v[idx] must be
 		// between lower[idx] and lower[idx]+size[idx]
-		for (int idx = 0; contains && idx < v.getDimension(); idx++)
-		{
+		for (int idx = 0; contains && idx < v.getDimension(); idx++) {
 			double value = v.get(idx);
 			double lower = this.lower.get(idx);
 			double upper = lower + size.get(idx);
@@ -26,23 +42,39 @@ public class RectBounds extends Bounds
 		return contains;
 	}
 
-	public Vector getLower()
-	{
+	/**
+	 * Returns the {@link Vector} representing this RectBounds' lower-left corner.
+	 * @return The {@link Vector} representing this RectBounds' lower-left corner.
+	 * @see Vector
+	 */
+	public Vector getLower() {
 		return lower;
 	}
 
-	public Vector getSize()
-	{
+	/**
+	 * Returns the {@link Vector} representing this RectBounds' upper-right corner, or size.
+	 * @return The {@link Vector} representing this RectBounds' lower-left corner.
+	 * @see Vector
+	 */
+	public Vector getSize() {
 		return size;
 	}
 
-	public void setLower(Vector lower)
-	{
+	/**
+	 * Sets the lower-left corner of this RectBounds to the specified {@link Vector}.
+	 * @param lower The {@link Vector} representing the RectBounds' new lower-left corner.
+	 * @see Vector
+	 */
+	public void setLower(Vector lower) {
 		this.lower = lower;
 	}
 
-	public void setSize(Vector size)
-	{
+	/**
+	 * Sets the size, or upper-right corner of this RectBounds to the specified {@link Vector}.
+	 * @param size The {@link Vector} representing the RectBounds' new upper-right corner.
+	 * @see Vector
+	 */
+	public void setSize(Vector size) {
 		this.size = size;
 	}
 
