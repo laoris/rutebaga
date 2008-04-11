b37e3430a0f6d3160fa060985f851252db8f302a
der.supermeister <der.supermeister@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-11 00:13:32 +0000

    ConcreteElementalList finished...probably.

--- trunk/src/rutebaga/controller/ConcreteElementalList.java
+++ trunk/src/rutebaga/controller/ConcreteElementalList.java
@@ -1,6 +1,8 @@
 package rutebaga.controller;
 
+import java.util.LinkedList;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 
 /**
@@ -24,26 +26,172 @@ import java.util.Iterator;
  */
 public class ConcreteElementalList implements ElementalList {
 
+	/**
+	 * The label of this ConcreteElementalList.
+	 */
+	private String label;
+	
+	/**
+	 * The list of ElementalLists for this ConcreteElementalList.  The
+	 * Iterator iterates over this list and then over the ListElements
+	 * within each ElementalList.
+	 */
+	private LinkedList<ElementalList> list = new LinkedList<ElementalList>();
+	
+	/**
+	 * Add a String to this ConcreteElementalList without a Command.
+	 * @param label the String element to add to this list
+	 */
+	public void add(String label) {
+		add(label, null);
+	}
+	
+	/**
+	 * Add a named Command to this ConcreteElementalList.  It is admissible
+	 * for the label to be null, in which case it will be replaced by the empty
+	 * String.
+	 * @param label the String element to add to this list
+	 * @param command the Command to associate with this list element
+	 */
 	public void add(String label, Command command) {
-		
+		// To hell with efficiency!
+		list.add(new SingleCommandElementalList(label, command));
 	}
 	
+	/**
+	 * Add an entire ElementalList as a component of this ConcreteElementalList.
+	 * When this ConcreteElementalList's iterator runs, it will visit each of
+	 * the specified ElementalList's ListElements in the order its Iterator
+	 * returns.
+	 * @param list the ElementalList to add as a child to this ConcreteElementalList
+	 */
 	public void add(ElementalList list) {
-		
+		if (list != null)
+			this.list.add(list);
 	}
 	
+	/* (non-Javadoc)
+	 * @see rutebaga.controller.ElementalList#getLabel()
+	 */
 	public String getLabel() {
-		// TODO Auto-generated method stub
-		return null;
+		return label;
 	}
 
-	public void setString() {
-		
+	/**
+	 * Sets the label of this ConcreteElementalList.
+	 * @param label the new label
+	 */
+	public void setLabel(String label) {
+		this.label = (label == null ? label : "");
 	}
 	
+	/**
+	 * Returns an Iterator over all the ListElements contained by this
+	 * ConcreteElementalList.  If an entire ElementalList has been added
+	 * as a child to this ConcreteElementalList, each of the child's 
+	 * ListElements will be returned by this Iterator.
+	 * @see ElementalList#iterator()
+	 * @see java.lang.Iterable#iterator()
+	 */
 	public Iterator<ListElement> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new ConcreteElementalListIterator();
 	}
 
+	/**
+	 * @author may
+	 *
+	 * A private Iterator that iterates through all the ElementalLists contained
+	 * by the parent ConcreteElementalList, visiting each of their ListElements
+	 * in turn.
+	 */
+	private class ConcreteElementalListIterator implements Iterator<ListElement> {
+		private Iterator<ElementalList> lists = ConcreteElementalList.this.list.iterator();
+		private Iterator<ListElement> currentList;
+		
+		/**
+		 * Determines if there is another unvisited ListElement in the current
+		 * ElementalList visited or an unvisited ElementalList contained by
+		 * this ConcreteElementalList.
+		 */
+		public boolean hasNext() {
+			return lists.hasNext() || currentList.hasNext();
+		}
+		
+		/**
+		 * Returns the next unvisited ListElement.
+		 * @return an unvisited ListElement
+		 */
+		public ListElement next() {
+			// currentList is spent, go to next
+			while (currentList == null || !currentList.hasNext()) {
+				// lists are spent...the end
+				if (!lists.hasNext())
+					throw new NoSuchElementException("No further ListElements");
+				currentList = lists.next().iterator();
+			}
+			return currentList.next();
+		}
+		
+		/**
+		 * Don't even think about it.
+		 */
+		public void remove() throws UnsupportedOperationException {
+			throw new UnsupportedOperationException("Bad! Bad!");
+		}
+	}
+	
+	/**
+	 * @author may
+	 *
+	 * A wrapper for a String-Command list element.  This ElementalList
+	 * contains a single ListElement, which is accessed via its Iterator,
+	 * that contains the label-command pair the SingleCommandElementalList
+	 * was constructed with. 
+	 */
+	private class SingleCommandElementalList implements ElementalList {
+
+		private String label;
+		private Command command;
+		
+		public SingleCommandElementalList(String label, Command command) {
+			this.label = (label == null ? label : "");
+			this.command = command;
+		}
+		
+		public String getLabel() {
+			return label;
+		}
+
+		public Iterator<ListElement> iterator() {
+			return new Iterator<ListElement>() {
+				private boolean visited = false;
+				
+				public boolean hasNext() {
+					return !visited;
+				}
+				
+				public ListElement next() {
+					if (visited)
+						throw new NoSuchElementException("Only one ListElement!");
+					else {
+						visited = true;
+						return new ListElement() {
+							public String getLabel() {
+								return SingleCommandElementalList.this.label;
+							}
+							
+							public Command getCommand() {
+								return SingleCommandElementalList.this.command;
+							}
+						};
+					}
+				}
+				
+				public void remove() throws UnsupportedOperationException {
+					throw new UnsupportedOperationException("Mommy, make the bad man stop ><");
+				}
+			};
+		}
+		
+	}
 }
