6f425d9e7f1f35ed1b095725bd1ba61cb054ed36
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-12 09:17:04 +0000

    javadoc - TileConvertor

--- src/rutebaga/model/environment/TileConvertor.java
+++ src/rutebaga/model/environment/TileConvertor.java
@@ -4,8 +4,32 @@ import java.util.Set;
 
 import rutebaga.commons.Vector;
 
+/**
+ * Provides a mapping between a tile-space and actual space.
+ * 
+ * @author Gary LosHuertos
+ * 
+ * @see Environment
+ */
 public interface TileConvertor
 {
+	/**
+	 * Determines the discrete tile-space coordinate of a coordinate in actual
+	 * space.
+	 * 
+	 * @param coordinate
+	 *            the coordinate in actual space
+	 * @return the tile-space coordinate
+	 */
 	Vector tileOf(Vector coordinate);
+
+	/**
+	 * Calculates the set of adjacent tiles to this tile based on the given
+	 * mapping's adjacency rules.
+	 * 
+	 * @param tile
+	 *            the tile-space coordinate upon which the calculation is based
+	 * @return the set of adjacent tile-space coordinates
+	 */
 	Set<Vector> adjacentTo(Vector tile);
 }
