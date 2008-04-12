0da53cec1b9ea644e7798203e7455e8541b350e5
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-12 09:10:52 +0000

    javadoc (brief) - InternalContainer

--- trunk/src/rutebaga/model/environment/InternalContainer.java
+++ trunk/src/rutebaga/model/environment/InternalContainer.java
@@ -3,20 +3,40 @@ package rutebaga.model.environment;
 import rutebaga.commons.ObjectUtils;
 import rutebaga.commons.Vector;
 
-public class InternalContainer
+/**
+ * This class is used as a workaround to Java's lack of the "internal" keyword.
+ * 
+ * By containing Location and PhysicsContainer (and declaring them final), any
+ * class outside of the environment package are prevented from interacting with
+ * instances of these classes.
+ * 
+ * More specifically, subclasses of Instance are prevented from interacting with
+ * the container components of the Instance, despite the fact that their
+ * accessors are available (due to protected semantics).
+ * 
+ * @author Gary LosHuertos
+ * 
+ */
+public final class InternalContainer
 {
-	protected static class Location
+	/**
+	 * Container class for an instance's location properties.
+	 * 
+	 * @author Gary LosHuertos
+	 * 
+	 */
+	protected static final class Location
 	{
 		private Vector coordinate;
 		private Vector tile;
 		private Environment environment;
 		private final Instance instance;
-		
+
 		public Location(Instance instance)
 		{
 			this.instance = instance;
 		}
-		
+
 		protected Instance getInstance()
 		{
 			return instance;
@@ -49,36 +69,40 @@ public class InternalContainer
 
 		protected void setTile(Vector tile)
 		{
-			if (!ObjectUtils.equals(this.tile, tile))
-			{
-				this.tile = tile;
-			}
+			this.tile = tile;
 		}
 
 	}
 
-	protected static class PhysicsContainer
+	/**
+	 * Container class for an instance's Newtonian properties.
+	 * 
+	 * @author Gary LosHuertos
+	 * 
+	 */
+	protected static final class PhysicsContainer
 	{
 		private Vector momentum = new Vector(0, 0);
 		private Vector appliedImpulse = new Vector(0, 0);
 		private Vector velocity = new Vector(0, 0);
 		private final Instance instance;
-		
+
 		public PhysicsContainer(Instance instance)
 		{
 			this.instance = instance;
 		}
-		
+
 		protected Instance getInstance()
 		{
 			return instance;
 		}
-		
+
 		protected void update(double friction)
 		{
 			double mass = instance.getMass();
-			this.velocity = momentum.plus(appliedImpulse).times(1/mass);
+			this.velocity = momentum.plus(appliedImpulse).times(1 / mass);
 			appliedImpulse = appliedImpulse.times(0);
+			//TODO add support for friction
 		}
 
 		public void applyImpulse(Vector impulse)
