ad27bf0320abe3dd3cf00a5b4e90187db475ccf9
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-12 09:06:25 +0000

    javadoc - InstanceSet

--- trunk/src/rutebaga/model/environment/InstanceSet.java
+++ trunk/src/rutebaga/model/environment/InstanceSet.java
@@ -2,6 +2,12 @@ package rutebaga.model.environment;
 
 import java.util.HashSet;
 
+/**
+ * Convenience wrapper.
+ * 
+ * @author Gary LosHuertos
+ *
+ */
 public class InstanceSet extends HashSet<Instance>
 {
 	private static final long serialVersionUID = 358791980720848943L;
