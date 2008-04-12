d30881d0e75aaef18f1a202d1ae87f6b831eb8f1
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-12 04:28:06 +0000

    environment updates

---
+++ src/rutebaga/test/model/environment/javastuff/TestInstance.java
@@ -0,0 +1,11 @@
+package rutebaga.test.model.environment.javastuff;
+
+import rutebaga.model.environment.Instance;
+
+public class TestInstance extends Instance
+{
+	public void doSomething()
+	{
+		
+	}
+}
