a03fbcb1d092e4607617013e724e1d68abe25dd2
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 23:46:31 +0000

    javadocs

--- src/rutebaga/view/drawer/Drawer.java
+++ src/rutebaga/view/drawer/Drawer.java
@@ -1,6 +1,5 @@
 package rutebaga.view.drawer;
 
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontMetrics;
@@ -9,36 +8,100 @@ import java.awt.Point;
 import java.awt.Shape;
 
 /**
- *
- * The Drawer abstract class allows for all information
- * being drawn out by the Windowing Toolkit to be
- * independent from the actual implementation of the
- * drawer (Graphics2D, Java3D, etc.). This allows
- * for reuse of the Windowing Toolkit on a variety
- * of mediums without having to revisit the actual
- * ViewComponents.
+ * 
+ * Allows for all information being drawn out by the Windowing Toolkit to be
+ * independent from the actual implementation of the Drawer (Graphics2D, Java3D,
+ * etc.). This allows for reuse of the Windowing Toolkit on a variety of mediums
+ * without having to revisit the actual
+ * {@link rutebaga.view.rwt.ViewComponent ViewComponents}.
+ * 
  * @author Ryan
  */
 public abstract class Drawer {
 
 	private Attribute attribute;
-	
-	public abstract void drawImage( Point p, Image img );
-	public abstract void drawRectangle( Point p, int width, int height );
-	public abstract void drawLine( Point begin, Point end );
-	public abstract void drawPoint( Point p );
-	public abstract void drawString( Point p, String string );
-	
-	protected abstract void setDrawColor( Color draw );
-	protected abstract void setFont( Font font );
-	protected abstract void setClipping( Shape clipping );
-	
+
+	/**
+	 * Draws an Image at specific location.
+	 * 
+	 * @param p
+	 *            Where the upper-left corner of the Image will be located.
+	 * @param img
+	 *            The Image to be drawn
+	 */
+	public abstract void drawImage(Point p, Image img);
+
+	/**
+	 * Draws a rectangle based on the current {@link Attribute Attriubtes}.
+	 * 
+	 * @param p
+	 *            Where the upper-left corner of the rectangle will be located.
+	 * @param width
+	 *            The width of the rectangle.
+	 * @param height
+	 *            The height of the rectangle.
+	 */
+	public abstract void drawRectangle(Point p, int width, int height);
+
+	/**
+	 * Draws a line based on the current {@link Attribute Attriubtes} from one
+	 * point to another.
+	 * 
+	 * @param begin
+	 *            One end of the line.
+	 * @param end
+	 *            One end of the line.
+	 */
+	public abstract void drawLine(Point begin, Point end);
+
+	/**
+	 * Draws a point based on the current {@link Attribute Attriubtes}.
+	 * 
+	 * @param p
+	 *            The location of the point.
+	 */
+	public abstract void drawPoint(Point p);
+
+	/**
+	 * Draws a text String based on the current {@link Attribute Attriubtes}.
+	 * 
+	 * @param p
+	 *            Where the lower-left corner of the text will be located.
+	 * @param string
+	 *            The String of text to be rendered.
+	 */
+	public abstract void drawString(Point p, String string);
+
+	protected abstract void setDrawColor(Color draw);
+
+	protected abstract void setFont(Font font);
+
+	protected abstract void setClipping(Shape clipping);
+
+	/**
+	 * Returns a FontMetrics based on the current {@link Attribute Attriubtes}.
+	 * 
+	 * @return A FontMetrics based on the current Font defined by the current
+	 *         Attribute.
+	 */
 	public abstract FontMetrics getFontMetrics();
-	
-	public void setAttribute( Attribute attr ) {
+
+	/**
+	 * Sets the current {@link Attribute Attriubtes}, discarding any previously
+	 * set Attriubtes. To set more than one Attribute remember to use a
+	 * {@link CompositeAttribute}.
+	 * 
+	 * @param attr
+	 *            The Attribute to set for this Drawer.
+	 */
+	public void setAttribute(Attribute attr) {
 		this.attribute = attr;
 	}
-	
+
+	/**
+	 * Returns the current {@link Attribute Attriubtes}.
+	 * @return The Attribute currently being used in this Drawer.
+	 */
 	public Attribute getAttribute() {
 		return attribute;
 	}
--- src/rutebaga/view/rwt/ViewComponent.java
+++ src/rutebaga/view/rwt/ViewComponent.java
@@ -8,47 +8,132 @@ import java.awt.event.MouseEvent;
 import rutebaga.commons.Bounds;
 import rutebaga.view.drawer.Drawer;
 
+/**
+ * Provides the base functionality of all components within the Windowing
+ * Toolkit. All ViewComponents have many states in common such as screen
+ * position(x, y), z-index, bounding boxes, and so on. This abstract class
+ * removes these basic functionalities of a component and encapsulates it all
+ * within the ViewComponent. Each sub-classing component can focus on how to
+ * draw itself rather than the tedium of maintaining its other states.
+ * <p>
+ * Responsibilities are to
+ * <ul>
+ * <li>Store screen position (x, y).
+ * <li>Store z-index to determine drawing order.
+ * <li> Maintain the bounds of the component (which can be used for culling or
+ * mouse hit detection).
+ * <li>If the bounds have changed, force a dirtyBounds() call up to its parents
+ * so they may recalculate their bounds.
+ * <li>Maintain the parent ViewComponent of this ViewComponent (if any).
+ * <li>Process key, mouse and mouseMotion events that are received from the
+ * EventDispatcher.
+ * 
+ * 
+ * @author Ryan
+ * 
+ */
 public abstract class ViewComponent {
 
 	private ViewComponent parent;
-	@SuppressWarnings("unused") //TODO
+	@SuppressWarnings("unused") // TODO
 	private boolean isVisible, hasFocus, dirtyBounds;
 	private Point screenPosition = new Point(0,0);
 	
+	/**
+	 * Renders this ViewComponent.
+	 * 
+	 * @param draw
+	 *            The {@link Drawer} to use for rendering.
+	 */
 	public abstract void draw( Drawer draw );
 
+	/**
+	 * Returns the parent ViewComponent of this ViewComponent, if it exists.
+	 * 
+	 * @return Parent ViewComponent of this ViewComponent if it exists,
+	 *         otherwise returns null.
+	 */
 	public ViewComponent getParent() {
 		return parent;
 	}
 	
+	/**
+	 * Returns the Point describing where this ViewComponent is located.
+	 * 
+	 * @return The Point representing the upper-left corner of this
+	 *         ViewComponent.
+	 */
 	public Point getLocation() {
 		return (Point) screenPosition.clone();
 	}
 	
+	/**
+	 * Returns the horizontal position of this ViewComponent. The same as
+	 * {@link #getLocation()}.x.
+	 * 
+	 * @return displacement of the left edge of this ViewComponent from the left
+	 *         edge of the View.
+	 */
 	public int getX() {
 		return screenPosition.x;
 	}
 	
+	/**
+	 * Returns the vertical position of this ViewComponent. The same as
+	 * {@link #getLocation()}.y.
+	 * 
+	 * @return displacement of the top edge of this ViewComponent from the top
+	 *         edge of the View.
+	 */
 	public int getY( ) {
 		return screenPosition.y;
 	}
 	
+	/**
+	 * Moves this ViewComponent to the specified Point.
+	 * 
+	 * @param p
+	 *            The new upper-left corner of this ViewComponent.
+	 */
 	public void setLocation( Point p ) {
 		this.screenPosition = p;
 	}
 	
+	/**
+	 * Moves this ViewComponent to the specified Point. The same as
+	 * setLocation(new Point(x,y)).
+	 * 
+	 * @param x
+	 *            The new displacement of the left edge of this ViewComponent
+	 *            from the left edge of the View.
+	 * @param y
+	 *            The new displacement of the top edge of this ViewComponent
+	 *            from the top edge of the View.
+	 */
 	public void setLocation( int x, int y ) {
 		screenPosition.setLocation(x, y);
 	}
 
+	/**
+	 * Sets the provided ViewComponent as the parent of this ViewComponent.
+	 * @param parent ViewComponent that will be set as this ViewComponent's parent.
+	 */
 	public void setParent(ViewComponent parent) {
 		this.parent = parent;
 	}
 
+	/**
+	 * Returns the current visibility of this ViewComponent.
+	 * @return True if this ViewComponent is currently visible.
+	 */
 	public boolean isVisible() {
 		return isVisible;
 	}
 
+	/**
+	 * Sets the visiblity of this ViewComponent.
+	 * @param visibility 
+	 */
 	public void setVisible(boolean visibility) {
 		this.isVisible = visibility;
 	}
@@ -62,7 +147,7 @@ public abstract class ViewComponent {
 	}
 	
 	public Bounds getBounds() {
-		//TODO
+		// TODO
 		return null;
 	}
 	
