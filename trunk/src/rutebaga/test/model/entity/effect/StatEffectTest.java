99942dcaa4e09a3fdfbe6d29bc48835b9d8aaff3
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 20:44:16 +0000

    initial import

---
+++ trunk/src/rutebaga/test/model/entity/effect/StatEffectTest.java
@@ -0,0 +1,42 @@
+package rutebaga.test.model.entity.effect;
+
+import rutebaga.model.entity.CharEntity;
+import rutebaga.model.entity.Entity;
+import rutebaga.model.entity.EntityEffect;
+import rutebaga.model.entity.EntityType;
+import rutebaga.model.entity.effect.StatEffect;
+import rutebaga.model.entity.stats.StatisticId;
+
+public class StatEffectTest
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		Entity entity = new CharEntity(new EntityType());
+		StatisticId stat = new StatisticId("some stat");
+		
+		System.out.println("before: " + entity.getStats().getValue(stat));
+		
+		StatEffect effect = new StatEffect(stat, 20);
+		Object id = entity.accept(effect);
+		
+		System.out.println("accepted, before tick: " + entity.getStats().getValue(stat));
+		
+		entity.tick();
+		
+		System.out.println("after tick: " + entity.getStats().getValue(stat));
+		
+		EntityEffect reverse = effect.getReverseEffect(id);
+		entity.accept(reverse);
+		
+		System.out.println("accepted reverse, before tick: " + entity.getStats().getValue(stat));
+		
+		entity.tick();
+		
+		System.out.println("after tick: " + entity.getStats().getValue(stat));
+	}
+
+}
