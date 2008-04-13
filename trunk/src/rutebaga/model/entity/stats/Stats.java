1adae1b04d6d70d2a02864a9c1e66751b12c39f1
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 20:32:55 +0000

    updated stats to accept modifications

--- trunk/src/rutebaga/model/entity/stats/Stats.java
+++ trunk/src/rutebaga/model/entity/stats/Stats.java
@@ -4,4 +4,6 @@ public interface Stats
 {
 	double getValue(StatisticId stat);
 	StatValue getStatObject(StatisticId stat);
+	void undo(Object id);
+	void modifyStat(StatModification modification, Object id);
 }
