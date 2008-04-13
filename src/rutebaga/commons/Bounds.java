354c6c80326b96e576980c0d05040f62a8f6d92e
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 20:34:34 +0000

    javadocs

--- src/rutebaga/commons/Bounds.java
+++ src/rutebaga/commons/Bounds.java
@@ -6,14 +6,13 @@ import java.util.Set;
 /**
  * Base class for a discrete bounds within space.
  * 
- * As mentioned, a bounds must be discrete; a bounds such as "all areas below
- * y=2" is not sufficient (as a location set could not possibly be generated).
+ * Bounds must be discrete; a bounds such as "all areas below y=2" is not
+ * sufficient (as a location set could not possibly be generated).
  * 
  * @author Gary LosHuertos
  * 
  */
-public abstract class Bounds
-{
+public abstract class Bounds {
 	/**
 	 * Tests whether or not the given vector is within the bounds.
 	 * 
@@ -27,24 +26,22 @@ public abstract class Bounds
 
 	/**
 	 * Tests if the {@link Vector Vectors} that are the columns in the provided
-	 * {@link Matrix} are contained by these Bounds and returns another Matrix
-	 * who's columns are only the {@link Vector Vectors} from the provided
-	 * {@link Matrix} that are contained in these Bounds.
+	 * {@link Matrix} are contained by these Bounds. Returns another Matrix
+	 * who's columns are only the Vectors from the provided passed Matrix that
+	 * are contained in these Bounds.
 	 * 
 	 * @param m
-	 *            {@link Matrix} who's columns are {@link Vector}s to be tested
-	 *            for containment in these Bounds.
-	 * @return The {@link Matrix} who's columns are {@link Vector}s that exist
-	 *         within these Bounds.
+	 *            {@link Matrix} who's columns are {@link Vector Vectors} to be
+	 *            tested for containment in these Bounds.
+	 * @return The {@link Matrix} who's columns are {@link Vector Vectors} that
+	 *         exist within these Bounds.
 	 * @see Matrix
 	 * @see Vector
 	 */
-	public Matrix filter(Matrix m)
-	{
+	public Matrix filter(Matrix m) {
 		Vector[] vectors = m.asVectors();
 
-		for (int idx = 0; idx < vectors.length; idx++)
-		{
+		for (int idx = 0; idx < vectors.length; idx++) {
 			if (!contains(vectors[idx]))
 				vectors[idx] = null;
 		}
@@ -53,18 +50,16 @@ public abstract class Bounds
 	}
 
 	/**
-	 * Filters the given set of vectors into a set containing only vectors
-	 * within this bounds.
+	 * Filters the given set of {@link Vector Vectors} into a set containing
+	 * only Vectors within this Bounds.
 	 * 
 	 * @param vectors
-	 *            the sample vector set
-	 * @return the vector set within the bounds
+	 *            The sample Vector set.
+	 * @return the Vector set within this Bounds.
 	 */
-	public Set<Vector> filter(Set<Vector> vectors)
-	{
+	public Set<Vector> filter(Set<Vector> vectors) {
 		Set<Vector> rval = new HashSet<Vector>();
-		for (Vector vector : vectors)
-		{
+		for (Vector vector : vectors) {
 			if (this.contains(vector))
 				rval.add(vector);
 		}
@@ -72,20 +67,18 @@ public abstract class Bounds
 	}
 
 	/**
-	 * Filters the given set of vectors into a set containing only vectors
-	 * within this bounds, displaced by an offset.
+	 * Filters the given set of {@link Vector Vectors} into a set containing
+	 * only Vectors within this Bounds, displaced by an offset.
 	 * 
 	 * @param vectors
-	 *            the sample vector set
+	 *            The sample Vector set.
 	 * @param offset
-	 *            the offset to use
-	 * @return the vector set within the bounds
+	 *            The offset to use.
+	 * @return The Vector set within this Bounds.
 	 */
-	public Set<Vector> filter(Set<Vector> vectors, Vector offset)
-	{
+	public Set<Vector> filter(Set<Vector> vectors, Vector offset) {
 		Set<Vector> rval = new HashSet<Vector>();
-		for (Vector vector : vectors)
-		{
+		for (Vector vector : vectors) {
 			if (this.contains(vector.plus(offset)))
 				rval.add(vector);
 		}
@@ -93,22 +86,26 @@ public abstract class Bounds
 	}
 
 	/**
-	 * Determines the rectangular bounding box of this bounds.
+	 * Determines the rectangular bounding box of this Bounds.
 	 * 
 	 * This bounding box is defined as the smallest surface that can fully
-	 * contain these bounds, for which each face must be orthogonal to or
-	 * parallel to every vector axis of the coordinate system.
+	 * contain this Bounds. Each face must be orthogonal to, or parallel to,
+	 * every {@link Vector} axis of this coordinate system.
 	 * 
-	 * @return the bounding box of this bounds
+	 * @return The bounding box of this Bounds.
 	 */
 	public abstract VectorRectangle getBoundingBox();
 
 	/**
+	 * Returns a set of the locations within this Bounds.
+	 * 
 	 * @param scale
-	 * @return
+	 *            Controls the resolution of the location set returned. For any
+	 *            {@link Vector} in the returned set, the closest another Vector
+	 *            can be is the distance of one basis Vector scaled by this scale.
+	 * @return A set of location Vectors that are within this Bounds.
 	 */
-	public Set<Vector> locationSet(double scale)
-	{
+	public Set<Vector> locationSet(double scale) {
 		Set<Vector> rval = getBoundingBox().locationSet(scale);
 		return filter(rval);
 	}
