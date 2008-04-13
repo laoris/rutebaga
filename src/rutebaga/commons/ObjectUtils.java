b6dab18395cbed6bbe28bc4a9b30dd562f9e207b
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 11:38:33 +0000

    javadoc - ObjectUtils

--- src/rutebaga/commons/ObjectUtils.java
+++ src/rutebaga/commons/ObjectUtils.java
@@ -1,12 +1,29 @@
 package rutebaga.commons;
 
+/**
+ * Utility class for statically accessed operations relating to objects.
+ * 
+ * @author Gary LosHuertos
+ * 
+ */
 public class ObjectUtils
 {
+	/**
+	 * Convenience method to avoid <code>NullPointerException</code>.
+	 * 
+	 * @param o1
+	 *            object to compare
+	 * @param o2
+	 *            object to compare
+	 * @return true if the objects are .equals() or both null, false otherwise
+	 * 
+	 * @see Object
+	 */
 	public static boolean equals(Object o1, Object o2)
 	{
-		if(o1 == null && o2 == null)
+		if (o1 == null && o2 == null)
 			return true;
-		if(o1 == null || o2 == null)
+		if (o1 == null || o2 == null)
 			return false;
 		return o1.equals(o2);
 	}
