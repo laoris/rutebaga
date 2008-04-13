e36434c05dfc154a33e5a02c5435a8b6be71a31a
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 11:15:34 +0000

    initial import

---
+++ src/rutebaga/model/environment/MovementEvent.java
@@ -0,0 +1,42 @@
+package rutebaga.model.environment;
+
+import rutebaga.commons.Vector;
+
+public class MovementEvent
+{
+	private final Instance instance;
+	private final Vector oldCoordinate;
+	private final Vector oldTile;
+
+	public MovementEvent(Instance instance, Vector oldCoordinate, Vector oldTile)
+	{
+		this.instance = instance;
+		this.oldCoordinate = oldCoordinate;
+		this.oldTile = oldTile;
+	}
+
+	public Instance getInstance()
+	{
+		return instance;
+	}
+
+	public Vector getNewCoordinate()
+	{
+		return instance.getCoordinate();
+	}
+
+	public Vector getNewTile()
+	{
+		return instance.getTile();
+	}
+	
+	public Vector getOldCoordinate()
+	{
+		return oldCoordinate;
+	}
+	
+	public Vector getOldTile()
+	{
+		return oldTile;
+	}
+}
