dc8eddd923fe672eb107e16d6a76b19eb45605d9
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 11:15:34 +0000

    initial import

---
+++ trunk/src/rutebaga/model/environment/MovementEventQueue.java
@@ -0,0 +1,22 @@
+package rutebaga.model.environment;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class MovementEventQueue
+{
+	private List<MovementEvent> events = new LinkedList<MovementEvent>();
+	
+	
+	public void onMovement(MovementEvent event)
+	{
+		this.events.add(event);
+	}
+	
+	public List<MovementEvent> poll()
+	{
+		List<MovementEvent> rval = events;
+		events = new LinkedList<MovementEvent>();
+		return rval;
+	}
+}
