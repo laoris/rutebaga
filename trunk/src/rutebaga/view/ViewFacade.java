93c98333a68c26f5687ec00435bd5a531bb66c08
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 21:05:55 +0000

    javadocs

--- trunk/src/rutebaga/view/ViewFacade.java
+++ trunk/src/rutebaga/view/ViewFacade.java
@@ -1,6 +1,5 @@
 package rutebaga.view;
 
-
 import rutebaga.commons.Vector;
 import rutebaga.controller.ElementalList;
 import rutebaga.controller.command.Command;
@@ -8,53 +7,165 @@ import rutebaga.view.rwt.ContextMenu;
 import rutebaga.view.rwt.TextFieldListener;
 import rutebaga.view.rwt.View;
 
+/**
+ * Provides a unified interface to manipulate the View Subsystem without
+ * intimate knowledge of how the View Subsystem works using the Facade pattern.
+ * <p>
+ * Responsibilities are to
+ * <ul>
+ * <li>Initialize the View system for rendering (includes setting up key, mouse
+ * and mouseMotion listeners)
+ * <li>Construct and tear down the view when transitioning between different
+ * states of the game (Title, AvatarCreation, GamePlay)
+ * <li>Manage the ContextMenu stack
+ * <li>Render the View for this current frame
+ * </ul>
+ * 
+ * @see Drawer
+ * @see ContextMenu
+ * @see ViewComponent
+ * @see EventDispatcher
+ * 
+ * 
+ * 
+ */
 public class ViewFacade {
-	
+
 	private View view;
 
-	public void constructView( int width, int height ) {
-		view = new View( width, height);
+	/**
+	 * Passes the parameters to the {@link rutebaga.view.rwt.View View}
+	 * Constructor.
+	 * 
+	 * @param width
+	 *            Width of the game.
+	 * @param height
+	 *            Height of the game.
+	 * @see rutebaga.view.rwt.View#View(int, int)
+	 */
+	public void constructView(int width, int height) {
+		view = new View(width, height);
 	}
-	
+
+	/**
+	 * Calls the {@link rutebaga.view.rwt.View View} constructor with the defaul
+	 * parameters (800,600) and then sets the View to full screen mode. Default
+	 * parameters are used to ensure the proportions of the View.
+	 * 
+	 * @see rutebaga.view.rwt.View#View(int, int)
+	 */
 	public void constructFullscreenView() {
 		view = new View(800, 600);
 		view.setFullscreen();
 	}
-	
+
+	/**
+	 * If the {@link rutebaga.view.rwt.View View} has been initialized, this
+	 * will call {@link rutebaga.view.rwt.View#renderFrame()}. Otherwise
+	 * nothing is done.
+	 */
 	public void renderFrame() {
-		if(view != null)
+		if (view != null)
 			view.renderFrame();
 	}
-	
-	public void createTitleScreen( ElementalList list ) {
-		
+
+	/**
+	 * Shows a title screen based on this ElementalList.
+	 * 
+	 * @param list
+	 *            A list of choises for the player.
+	 */
+	public void createTitleScreen(ElementalList list) {
+
 	}
-	
-	public void createAvatarCreationScreen( TextFieldListener listener, ElementalList list, Command accept, Command cancel ) {
-		
+
+	/**
+	 * Shows a screen where the user can create a player.
+	 * 
+	 * @param listener
+	 *            A listener for the typed input.
+	 * @param list
+	 *            A list of choices for the player.
+	 * @param accept
+	 *            The {@link Command} for what to do on confirmation.
+	 * @param cancel
+	 *            The {@link Command} for what to do on cancelation.
+	 */
+	public void createAvatarCreationScreen(TextFieldListener listener,
+			ElementalList list, Command accept, Command cancel) {
+
 	}
-	
-	public ContextMenu createRootContextMenu( ElementalList list, Vector vector ) {
+
+	/**
+	 * Spawns a ContextMenu at the {@link Vector} location provided.
+	 * 
+	 * @param list
+	 *            A list of choices for the player.
+	 * @param vector
+	 *            The location to spawn the menu.
+	 * @return The ContextMenu that was created.
+	 */
+	public ContextMenu createRootContextMenu(ElementalList list, Vector vector) {
 		return null;
 	}
-	
-	public ContextMenu createSubContextMenu( ElementalList list ) {
+
+	/**
+	 * Opens up a ContextMenu when a button in an existing ContextMenu is
+	 * pressed.
+	 * 
+	 * @param list
+	 *            A list of choices for the player.
+	 * @return The ContextMenu that was created.
+	 */
+	public ContextMenu createSubContextMenu(ElementalList list) {
 		return null;
 	}
-	
-	public ContextMenu createScrollMenu( ElementalList list, int pageSize ) {
+
+	/**
+	 * In a ContextMenu, shows a scrollable menu capable of displaying mass
+	 * information like player statistics or inventory.
+	 * 
+	 * @param list
+	 *            A list of choices for the player.
+	 * @param pageSize
+	 *            The amount of information per scrollable page.
+	 * @return The ContextMenu that was created.
+	 */
+	public ContextMenu createScrollMenu(ElementalList list, int pageSize) {
 		return null;
 	}
-	
-	public ContextMenu createDialogMenu( ElementalList list, Vector vector ) {
+
+	/**
+	 * Spawns a DialogMenu at the provided {@link Vector} location.
+	 * 
+	 * @param list
+	 *            A list of choices for the player.
+	 * @param vector
+	 *            The location at which to spawn this menu.
+	 * @return The ContextMenu that was created.
+	 */
+	public ContextMenu createDialogMenu(ElementalList list, Vector vector) {
 		return null;
 	}
-	
-	public void createWarningBox( ElementalList list ) {
-		
+
+	/**
+	 * Shows a warning to the player.
+	 * 
+	 * @param list
+	 *            A list of items to be presented to the player.
+	 */
+	public void createWarningBox(ElementalList list) {
+
 	}
-	
-	public void closeContextMenu( ContextMenu menu ) {
-		
+
+	/**
+	 * Closes a {@link ContextMenu}. This could either be the root, or
+	 * somewhere in the menu heirarchy.
+	 * 
+	 * @param menu
+	 *            A ContextMenu to be closed.
+	 */
+	public void closeContextMenu(ContextMenu menu) {
+
 	}
 }
