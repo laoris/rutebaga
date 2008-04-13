a321e622d57681c0f1eb4f3c49b76038cc28a24b
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-13 09:58:38 +0000

    modified javadoc (added bounds explanation)

--- src/rutebaga/commons/NumberUtils.java
+++ src/rutebaga/commons/NumberUtils.java
@@ -2,17 +2,26 @@ package rutebaga.commons;
 
 /**
  * Simple numeric methods.
+ * 
  * @author Gary
- *
+ * 
  */
 public class NumberUtils
 {
 	/**
-	 * Returns true if the value given falls between the maximum and minimum values provided.
-	 * @param value The number to test.
-	 * @param min The minimum value.
-	 * @param max The maximum value.
-	 * @return The boolean that is true if the value is greater than min and less than max.
+	 * Returns true if the value given falls between the maximum and minimum
+	 * values provided.  The test is lower-bound inclusive, and upper-bound exclusive:
+	 * 
+	 * [ min, max )
+	 * 
+	 * @param value
+	 *            The number to test.
+	 * @param min
+	 *            The minimum value.
+	 * @param max
+	 *            The maximum value.
+	 * @return The boolean that is true if the value is greater than or equal to
+	 *         min and less than max.
 	 */
 	public static boolean between(double value, double min, double max)
 	{
