651ca40fb50cb6633d76c6ee91c69f0b79eaa09a
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-14 03:03:57 +0000

    javadocs

--- trunk/src/rutebaga/view/rwt/TextFieldComponent.java
+++ trunk/src/rutebaga/view/rwt/TextFieldComponent.java
@@ -2,12 +2,32 @@ package rutebaga.view.rwt;
 
 import rutebaga.view.drawer.Drawer;
 
+/**
+ * Captures user keyboard input for display on screen. The captured information
+ * can potentially be used somewhere else by whomever has attached a
+ * {@link TextFieldListener} to the TextFieldComponent.
+ * <p>
+ * Responsibilities are to
+ * <ul>
+ * <li>
+ * 
+ * If they TextField has key focus, capture key inputs and display them within
+ * the TextField.
+ * <li>Whenever something is being typed into the TextField, notify its
+ * listeners of the changes.
+ * <li>Store the current state of the string within the TextField.
+ * </ul>
+ * 
+ * 
+ * @author Ryan
+ * 
+ */
 public class TextFieldComponent extends ViewComponent {
 
 	@Override
 	public void draw(Drawer draw) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
