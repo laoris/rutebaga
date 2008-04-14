93ffd76c04e5a39fab38ab4cd63dfe098390e1ea
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-14 02:21:11 +0000

    javadocs

--- trunk/src/rutebaga/view/rwt/ComponentDecorator.java
+++ trunk/src/rutebaga/view/rwt/ComponentDecorator.java
@@ -2,16 +2,42 @@ package rutebaga.view.rwt;
 
 import rutebaga.view.drawer.Drawer;
 
+/**
+ * Provides the basic functionality of decorating a ViewComponent with
+ * additional embellishments (for example, ScrollBars ).
+ * 
+ * The ComponentDecorator is different from that of
+ * {@link ViewCompositeComponent} in primarily three ways:
+ * <p>
+ * <ul>
+ * <li> The ComponentDecorator does not have a parent type relationship with the
+ * object that it is decorating.
+ * <li>The ComponentDecorator does not redefine the coordinate system for the
+ * component it is decorating, it chooses how to position the decorate component
+ * based on its own internal formatting strategy.
+ * <li>The ComponentDecorator implies more visual information will be rendered
+ * to the screen than merely the ViewComponent.
+ * 
+ * @author Ryan
+ * 
+ */
 public abstract class ComponentDecorator extends ViewComponent {
-	
+
 	private final ViewComponent decorated;
 
-	public ComponentDecorator( ViewComponent decorated ) {
+	/**
+	 * Constructs a ComponentDecorator for decorating the specified
+	 * ViewComponent.
+	 * 
+	 * @param decorated
+	 *            The ViewComponent to be decorated.
+	 */
+	public ComponentDecorator(ViewComponent decorated) {
 		this.decorated = decorated;
 	}
-	
+
 	public void draw(Drawer draw) {
-		decorated.draw( draw );
+		decorated.draw(draw);
 	}
 
 }
--- trunk/src/rutebaga/view/rwt/ContextMenu.java
+++ trunk/src/rutebaga/view/rwt/ContextMenu.java
@@ -1,7 +1,35 @@
 package rutebaga.view.rwt;
 
+/**
+ * Provides a simple way to create interactable menus. These ContextMenus send
+ * {@link rutebaga.controller.command.Command Commands} to the model. Since
+ * ContextMenus decorate a ViewComponent, any information can be contained
+ * within a ContextMenu, not just options.
+ * <p>
+ * ContextMenus will entail some of the following:
+ * <ul>
+ * <li> Inventory ContextMenu (a scroll decorated list of items with a button
+ * next to each item that will trigger a new ItemOptionsContextMenu).
+ * <li>ItemOptions ContextMenu ( a list of buttons to equip, use, drop, unequip
+ * etc that is context based on the item ).
+ * </ul>
+ * <p>
+ * Responsibilities are to
+ * <ul>
+ * <li>
+ * 
+ * Forward events to the decorated object and the sub-ContextMenu (if any).
+ * <LI> Maintain the sub-ContextMenu.
+ * 
+ * @author Ryan
+ * 
+ */
 public class ContextMenu extends ComponentDecorator {
 
+	/**
+	 * Contructs a ContextMenu as a decorator of the specified ViewComponent.
+	 * @param decorated The ViewComponent to be decorated.
+	 */
 	public ContextMenu(ViewComponent decorated) {
 		super(decorated);
 		// TODO Auto-generated constructor stub
