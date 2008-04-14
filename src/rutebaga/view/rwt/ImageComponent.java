ad6901ecb13bd2180813579e3c7b4ae246f5494c
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-14 03:03:57 +0000

    javadocs

--- src/rutebaga/view/rwt/ImageComponent.java
+++ src/rutebaga/view/rwt/ImageComponent.java
@@ -4,14 +4,24 @@ import java.awt.Image;
 
 import rutebaga.view.drawer.Drawer;
 
+/**
+ * Allows for Images to be drawn to the screen.
+ * 
+ * @author Ryan
+ * 
+ */
 public class ImageComponent extends ViewComponent {
-	
+
 	private Image image;
 
-	public ImageComponent( Image image ) {
+	/**
+	 * Constructs a new ImageComponent for viewing the specified Image.
+	 * @param image The Image that this ImageComponent should draw.
+	 */
+	public ImageComponent(Image image) {
 		this.image = image;
 	}
-	
+
 	public void draw(Drawer draw) {
 		draw.drawImage(this.getLocation(), image);
 	}
--- src/rutebaga/view/rwt/TextLabelComponent.java
+++ src/rutebaga/view/rwt/TextLabelComponent.java
@@ -1,6 +1,5 @@
 package rutebaga.view.rwt;
 
-
 import java.awt.Color;
 import java.awt.Font;
 
@@ -9,40 +8,67 @@ import rutebaga.view.drawer.CompositeAttribute;
 import rutebaga.view.drawer.Drawer;
 import rutebaga.view.drawer.FontAttribute;
 
+/**
+ * Provides the simple ability of displaying a
+ * String to the view with whatever Font or Color attributes that have been
+ * specified.
+ * 
+ * @author Ryan
+ * 
+ */
 public class TextLabelComponent extends ViewComponent {
-	
+
 	private String label;
+
 	private FontAttribute font;
+
 	private ColorAttribute color;
+
 	private CompositeAttribute composite;
-	
+
 	private TextLabelComponent() {
 		font = new FontAttribute(new Font("Arial", Font.PLAIN, 10));
-		color = new ColorAttribute( Color.BLACK );
+		color = new ColorAttribute(Color.BLACK);
 		composite = new CompositeAttribute();
-		composite.addAttribute( font );
-		composite.addAttribute( color );
+		composite.addAttribute(font);
+		composite.addAttribute(color);
 	}
 
-	public TextLabelComponent( String label ) {
+	/**
+	 * Constructs a new TextLabelComponent for displaying the specified String.
+	 * @param label Text to display.
+	 */
+	public TextLabelComponent(String label) {
 		this();
 		this.label = label;
 	}
-	
-	public void draw( Drawer draw ) {
-		draw.setAttribute( composite );
+
+	public void draw(Drawer draw) {
+		draw.setAttribute(composite);
 		draw.drawString(this.getLocation(), label);
 	}
-	
-	public void setFont( Font font ) {
-		this.font.setFont( font );
+
+	/**
+	 * Changes the Font used to draw this TextLabelComponent.
+	 * @param font The font to use.
+	 */
+	public void setFont(Font font) {
+		this.font.setFont(font);
 	}
-	
-	public void setFontColor( Color color ) {
-		this.color.setColor( color );
+
+	/**
+	 * Changes the Color to draw this TextLabelComponent in.
+	 * @param color The new Color of the text.
+	 */
+	public void setFontColor(Color color) {
+		this.color.setColor(color);
 	}
-	
-	public void setLabel( String label ) {
+
+	/**
+	 * Changes the text stored in this TextLabelComponent.
+	 * @param label The new text.
+	 */
+	public void setLabel(String label) {
 		this.label = label;
 	}
 
