d3e6172ecda0a9cc2e9358a2877c52158505fbb2
der.supermeister@gmail.com <der.supermeister@gmail.com@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-15 23:08:35 +0000

    Added ShopkeeperInventoryCommandFactory and removed StaticCommandFactory, making ConcreteElementalList implement CommandFactory<Object>.

--- src/rutebaga/controller/command/StaticCommandFactory.java
+++
@@ -1,8 +0,0 @@
-package rutebaga.controller.command;
-
-
-public class StaticCommandFactory<T> extends ConcreteElementalList implements CommandFactory<T> {
-	public ElementalList getCommandListFor(T element) {
-		return this;
-	}
-}
