38f2a25f61b0d1adc35a2b9037be9bb961540042
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-15 22:06:15 +0000

    javadocs

--- trunk/src/rutebaga/model/entity/stats/Stats.java
+++ trunk/src/rutebaga/model/entity/stats/Stats.java
@@ -4,13 +4,61 @@ import java.util.Set;
 
 public interface Stats
 {
+	/**
+	 * Gets the current value of the statistic.
+	 * 
+	 * @param stat
+	 *            the id of the statistic whose value should be returned
+	 * @return the current value of the stat
+	 */
 	double getValue(StatisticId stat);
 
+	/**
+	 * Gets the actual StatValue object for the given statistic id.
+	 * 
+	 * @param stat
+	 *            the id of the statistic whose StatValue should be returned
+	 * @return the StatValue for the stat
+	 * 
+	 * @see StatValue
+	 */
 	StatValue getStatObject(StatisticId stat);
 
+	/**
+	 * Reverse the effect of the stat modification with the given ID. This ID
+	 * must have been given to the modifyStat operation.
+	 * 
+	 * This operation is not guaranteed to be stable for IDs that have either
+	 * not been used with this Stats object, or for IDs representing
+	 * modifications that have already been undone.
+	 * 
+	 * @param id
+	 *            the ID for the modification to reverse
+	 *            
+	 * @see StatModification
+	 */
 	void undo(Object id);
 
+	/**
+	 * Modifies a statistic according to a StatModification, assigning an
+	 * identifier to the modification so that it can be undone later.
+	 * 
+	 * This operation is not guaranteed to be stable for reused IDs.
+	 * 
+	 * @param modification
+	 *            the modification to perform
+	 * @param id
+	 *            the identifier to use for the modification
+	 *            
+	 * @see StatModification
+	 */
 	void modifyStat(StatModification modification, Object id);
-	
+
+	/**
+	 * @return the identifiers of the statistics currently supported by this
+	 *         Stats object
+	 *         
+	 * @see StatisticId
+	 */
 	Set<StatisticId> getStatIds();
 }
