309b3d391a7d8b1113bd506610070b2477eee730
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-16 03:25:55 +0000

    fixed size method

--- trunk/src/rutebaga/test/model/environment/InstanceSetTest.java
+++ trunk/src/rutebaga/test/model/environment/InstanceSetTest.java
@@ -4,6 +4,11 @@ import rutebaga.model.entity.CharEntity;
 import rutebaga.model.entity.Entity;
 import rutebaga.model.environment.Instance;
 import rutebaga.model.environment.ConcreteInstanceSet;
+import rutebaga.model.environment.InstanceFilter;
+import rutebaga.model.environment.InstanceSet;
+import rutebaga.model.environment.InstanceSetIdentifier;
+import rutebaga.model.item.Item;
+import rutebaga.model.map.Tile;
 
 public class InstanceSetTest
 {
@@ -11,11 +16,115 @@ public class InstanceSetTest
 	{
 		ConcreteInstanceSet instanceSet = new ConcreteInstanceSet();
 		
-		Entity e = new CharEntity();
+		instanceSet.add(new CharEntity());
+		instanceSet.add(new CharEntity());
+		instanceSet.add(new CharEntity());
+		instanceSet.add(new CharEntity());
 		
-		System.out.println(instanceSet.add(e));
+		instanceSet.add(new Tile());
+		instanceSet.add(new Tile());
+		instanceSet.add(new Tile());
+		
+		instanceSet.add(new Item(null){
+
+			@Override
+			public boolean blocks(Instance other)
+			{
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public double getFriction()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public double getLayer()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public double getMass()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public void tick()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
+		
+
+		instanceSet.add(new Item(null){
+
+			@Override
+			public boolean blocks(Instance other)
+			{
+				// TODO Auto-generated method stub
+				return false;
+			}
+
+			@Override
+			public double getFriction()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public double getLayer()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public double getMass()
+			{
+				// TODO Auto-generated method stub
+				return 0;
+			}
+
+			@Override
+			public void tick()
+			{
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
 		System.out.println(instanceSet.size());
+		
+		for(Instance instance : instanceSet)
+		{
+			System.out.println(instance.getClass());
+		}
+		
+		System.out.println();
+		
+		InstanceFilter filter =  new InstanceFilter(InstanceSetIdentifier.ENTITY, InstanceSetIdentifier.TILE);
+		
+		InstanceSet filtered = filter.filter(instanceSet);
+		
+		System.out.println(filtered.size());
+		
+		for(Instance instance : filtered)
+		{
+			System.out.println(instance.getClass());
+		}
+		
+		System.out.println();
 		
 		for(Instance instance : instanceSet)
 		{
