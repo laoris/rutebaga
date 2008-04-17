7d871b70be42ab3606b21209e7120c6506d7571f
rcobb85 <rcobb85@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-17 01:26:05 +0000

    Added a View Component test class.

---
+++ trunk/src/rutebaga/test/view/ViewComponentTest.java
@@ -0,0 +1,42 @@
+package rutebaga.test.view;
+
+import java.awt.Color;
+
+import rutebaga.view.game.FPSTextComponent;
+import rutebaga.view.rwt.*;
+
+public class ViewComponentTest {
+
+
+		private static int SCREENWIDTH = 800, SCREENHEIGHT = 600;
+
+
+		public static void main(String args[])
+		{
+			View view = new View(SCREENWIDTH, SCREENHEIGHT);
+			//view.setFullscreen();
+
+			FPSTextComponent fps = new FPSTextComponent();
+			fps.setLocation(100, 100);
+			fps.setFontColor(Color.RED);	
+			view.addViewComponent(fps);
+			
+			ViewCompositeComponent vcc = new ViewCompositeComponent();
+			vcc.addChild(new ButtonComponent("test1"));
+			vcc.addChild(new ButtonComponent("test2"));
+			vcc.addChild(new ButtonComponent("test3"));
+			vcc.addChild(new ButtonComponent("test4"));
+			vcc.addChild(new ButtonComponent("test5"));
+		
+			
+			ScrollDecorator scroll = new ScrollDecorator(vcc, 200, 50);
+			scroll.setLocation(200, 200);
+			
+			view.addViewComponent(scroll);
+			
+			while(true)
+				view.renderFrame();
+
+		}
+
+}
