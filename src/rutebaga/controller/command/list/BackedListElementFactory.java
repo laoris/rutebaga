d114ad6a16967cb47257169e3a1150630c0d9484
der.supermeister@gmail.com <der.supermeister@gmail.com@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-20 23:00:33 +0000

    BackedListElementFactory now requires a UIFacade so it can create context menus.

--- src/rutebaga/controller/command/list/BackedListElementFactory.java
+++ src/rutebaga/controller/command/list/BackedListElementFactory.java
@@ -4,20 +4,25 @@ import rutebaga.controller.command.Command;
 import rutebaga.controller.command.CommandFactory;
 import rutebaga.controller.command.CreateSubContextMenuCommand;
 import rutebaga.model.Named;
+import rutebaga.view.UserInterfaceFacade;
 
 public class BackedListElementFactory<T extends Named> implements ListElementFactory<T> {
 
 	private final CommandFactory<T> factory;
+	private final UserInterfaceFacade facade;
 	
-	public BackedListElementFactory(CommandFactory<T> commands) {
-		factory = commands;
+	public BackedListElementFactory(CommandFactory<T> commands, UserInterfaceFacade facade) {
+		this.factory = commands;
+		this.facade = facade;
 	}
 	
 	public ListElement makeElement(final T element) {
 		// TODO: cache old ListElements here?  Or maybe in the DynamicElementalList itself...
 		return new ListElement() {
 			public Command getCommand() {
-				return new CreateSubContextMenuCommand(factory.getCommandListFor(element));
+				CreateSubContextMenuCommand command = new CreateSubContextMenuCommand(factory.getCommandListFor(element));
+				command.setUIFacade(facade);
+				return command;
 			}
 			
 			public String getLabel() {
