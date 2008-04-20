261f9043e9db19f2b8b1a7a024c819d7d285b353
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-20 00:59:13 +0000

    slots

---
+++ src/rutebaga/model/entity/effect/SlotEffect.java
@@ -0,0 +1,32 @@
+package rutebaga.model.entity.effect;
+
+import rutebaga.model.entity.Entity;
+import rutebaga.model.entity.EntityEffect;
+import rutebaga.model.entity.ReversibleEntityEffect;
+import rutebaga.model.item.SlotType;
+
+public class SlotEffect extends ReversibleEntityEffect
+{
+	private SlotType type;
+	private int qty;
+
+	public SlotEffect(SlotType type, int qty)
+	{
+		super();
+		this.type = type;
+		this.qty = qty;
+	}
+
+	@Override
+	public EntityEffect getReverseEffect(Object id)
+	{
+		return new SlotEffect(type, -qty);
+	}
+
+	@Override
+	protected void affect(Entity entity, Object id)
+	{
+		entity.getInventory().getCurrentAllocations().add(type, qty);
+	}
+
+}
