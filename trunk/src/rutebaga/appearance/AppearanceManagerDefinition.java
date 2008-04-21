55d72cecd375cca0f3e7c8e44c50e871257c2d25
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-21 04:19:32 +0000

    javadocs

--- trunk/src/rutebaga/appearance/AppearanceManagerDefinition.java
+++ trunk/src/rutebaga/appearance/AppearanceManagerDefinition.java
@@ -3,7 +3,15 @@ package rutebaga.appearance;
 import rutebaga.model.environment.Instance;
 import rutebaga.model.environment.appearance.AppearanceManager;
 
+/**
+ *	Knows how to construct an Appearance manager for the paramaterized Instance.
+ * @param <T>
+ */
 public interface AppearanceManagerDefinition<T extends Instance>
 {
+	/**
+	 * @param instance The Instance to be represented by the returned AppearanceManager.
+	 * @return A new AppearanceManager for the provided instance.
+	 */
 	AppearanceManager make(T instance);
 }
