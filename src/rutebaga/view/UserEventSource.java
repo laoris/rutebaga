0476b81337fcffa1a3006242e6b863bc51cbf8d9
fluffyzzz <fluffyzzz@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-21 04:45:23 +0000

    docs

--- src/rutebaga/view/UserEventSource.java
+++ src/rutebaga/view/UserEventSource.java
@@ -3,6 +3,10 @@ package rutebaga.view;
 import java.awt.event.KeyListener;
 import java.awt.event.MouseListener;
 
+/**
+ * An interface for listening to events from the keyboard and mouse.
+ *
+ */
 public interface UserEventSource {
 	void addKeyListener(KeyListener kl );
 	
