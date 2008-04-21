289d3c6342d5fc54cac28789e2f19fece9d60edd
gary.loshuertos <gary.loshuertos@04bda297-8649-0410-b8b1-9d8bffe7626b>
2008-04-21 02:00:04 +0000

    initial import

---
+++ src/rutebaga/model/entity/Damage.java
@@ -0,0 +1,44 @@
+package rutebaga.model.entity;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import rutebaga.commons.math.SymbolicFunction;
+import rutebaga.model.entity.effect.StatEffect;
+import rutebaga.model.entity.stats.StatisticId;
+
+/**
+ * Classification of damage intended to be carried out upon an Entity.
+ * 
+ * Execution results in the execution of a StatEffect upon the entity.
+ * The returned token for this effect is provided.
+ * 
+ * Damage has three components:
+ * 
+ * - A symbolic function that provides the magnitude of the change
+ *   to the statistic;<br/>
+ * - The classification of this damage;
+ * - The statistic to be damaged
+ * 
+ * The classification is not strictly necessary, but is provided so that
+ * magnitude providers can be reused.
+ * 
+ * @author Gary LosHuertos
+ *
+ */
+public class Damage
+{
+	private DamageType type;
+	private SymbolicFunction<Entity> magnitudeProvider;
+	private StatisticId damagedStat;
+	
+	public Object execute(Entity entity, double amount)
+	{
+		Map<String, Double> symT = new HashMap<String, Double>();
+		symT.put("amount", amount);
+		symT.put("defense", entity.getDamageResistance().getValue(type));
+		double statAffectAmount = magnitudeProvider.getValue(symT, entity);
+		EntityEffect eff = new StatEffect(damagedStat, statAffectAmount);
+		return entity.accept(eff);
+	}
+}
